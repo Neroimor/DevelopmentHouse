ba103900107e7ef7fa2dea148eb63f49dd43f1c6
Neroimor <neroimor@gmail.com>
2025-04-15 20:22:53 +0300

    Add project files.

---
+++ DTO/DataLinks.cs
@@ -0,0 +1,8 @@
+﻿namespace MyDevelopmentHouse.DTO
+{
+    public class DataLinks
+    {
+        public string NameReference { get; set; } = string.Empty;
+        public string UrlReference { get; set; } = string.Empty;
+    }
+}
